c3edb93527505cb58776435d3ffb37213eec9ee8
SRINITHYA T <139114296+SRINITHYA04@users.noreply.github.com>
2026-05-01 09:54:07 +0000

    feat: implement PATCH /books/{book_id} for partial updates using optional fields and exclude_unset

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
-from typing import List #It helps you describe data types more clearly 
+from typing import List, Optional #It helps you describe data types more clearly 
 
 app = FastAPI(title="Crud FastApi_Crud_Application")
 
@@ -62,7 +62,7 @@ books = [
 ]
 
 
-class Book(BaseModel):
+class Book_Strict(BaseModel):
     id: int
     title: str
     author: str
@@ -71,28 +71,53 @@ class Book(BaseModel):
     page_count: int
     language: str
 
+class Book_Flexible(BaseModel):
+    title: Optional[str] = None
+    author: Optional[str] = None
+    publisher: Optional[str] = None
+    published_date: Optional[str] = None
+    page_count: Optional[int] = None
+    language: Optional[str] = None
+
+    # 1. Optional[str] -> Value can be str or None 
+    # 2. = None -> Field is not required in request and  
+    #           -> If not provided → it defaults to None
+
 
 # 1. get the list of all books
-@app.get("/books", response_model = List[Book]) #decorator
-def get_all_books()-> List[Book]:
+@app.get("/books", response_model = List[Book_Strict]) #decorator
+def get_all_books()-> List[Book_Strict]:
     return books
 
 #2. Add a new to the In memory db
 @app.post("/books", status_code = status.HTTP_201_CREATED)
-async def create_a_book(Book_data : Book): # Book_data is of obj type
+async def create_a_book(Book_data : Book_Strict): # Book_data is of obj type
     new_book = Book_data.model_dump() # dump_modle() converts obj to dict. so new_bbok is of dict type
     books.append(new_book) # append makes no error
     return new_book
 
 # the newly add book can de veiwed but again giving get/books.
 
-# 3. Retrive a Book from the in memory DB
+# 3. Retrive a Book from the in-memory DB
 @app.get('/books/{book_id}')
-async def get_book(book_id: int):
+async def get_book(book_id: int)-> dict :
     for book in books:
         if book["id"] == book_id:
              return book
     raise HTTPException(status_code =404, detail="Book not found")
-    
+
+
+#  4. Update a Book in the in-memory DB
+@app.patch('/books/{book_id}')
+async def Update_book(book_id: int, book_update_data: Book_Flexible):
+    for book in books:
+        if book["id"] == book_id:
+            book.update(book_update_data.model_dump(exclude_unset=True))
+            return book
+    raise HTTPException(status_code =404, detail="Book not found") 
+
+
+#5. 
+
 
 
