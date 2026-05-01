4f41171ab30dbc2bbf53aa8caaa086613e0e3464
SRINITHYA T <139114296+SRINITHYA04@users.noreply.github.com>
2026-05-01 11:15:44 +0530

    Feat: Initialize FastAPi CRUD with a in-memory DB

---
+++ app.py
@@ -0,0 +1,74 @@
+books = [
+    {
+        "id": 1,
+        "title": "Atomic Habits",
+        "author": "James Clear",
+        "publisher": "Avery",
+        "published_date": "2018-10-16",
+        "page_count": 320,
+        "language": "English"
+    },
+    {
+        "id": 2,
+        "title": "The Alchemist",
+        "author": "Paulo Coelho",
+        "publisher": "HarperOne",
+        "published_date": "1988-01-01",
+        "page_count": 208,
+        "language": "English"
+    },
+    {
+        "id": 3,
+        "title": "Rich Dad Poor Dad",
+        "author": "Robert Kiyosaki",
+        "publisher": "Plata Publishing",
+        "published_date": "1997-04-01",
+        "page_count": 336,
+        "language": "English"
+    },
+    {
+        "id": 4,
+        "title": "Ikigai",
+        "author": "Héctor García",
+        "publisher": "Penguin",
+        "published_date": "2016-01-01",
+        "page_count": 208,
+        "language": "English"
+    },
+    {
+        "id": 5,
+        "title": "Deep Work",
+        "author": "Cal Newport",
+        "publisher": "Grand Central Publishing",
+        "published_date": "2016-01-05",
+        "page_count": 304,
+        "language": "English"
+    },
+    {
+        "id": 6,
+        "title": "The Power of Now",
+        "author": "Eckhart Tolle",
+        "publisher": "New World Library",
+        "published_date": "1997-01-01",
+        "page_count": 236,
+        "language": "English"
+    },
+    {
+        "id": 7,
+        "title": "Think and Grow Rich",
+        "author": "Napoleon Hill",
+        "publisher": "The Ralston Society",
+        "published_date": "1937-01-01",
+        "page_count": 238,
+        "language": "English"
+    },
+    {
+        "id": 8,
+        "title": "Zero to One",
+        "author": "Peter Thiel",
+        "publisher": "Crown Business",
+        "published_date": "2014-09-16",
+        "page_count": 224,
+        "language": "English"
+    }
+]
